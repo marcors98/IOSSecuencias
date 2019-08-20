59bcc962ef5d1323842b7595e4f749fc6552122e
Alumno <ulsa@MARCO-ANTONIO.local>
2019-08-20 14:32:58 -0700

    secuencias

--- secuencias/ViewController.swift
+++ secuencias/ViewController.swift
@@ -10,9 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func doTapConejo(_ sender: Any) {
+        if imgSecuencia.isAnimating {
+            imgSecuencia.stopAnimating()
+        } else {
+            imgSecuencia.startAnimating()
+        }
+        
+    }
+    @IBOutlet weak var imgSecuencia: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //Arreglo de UIImage con las imagenes de la secuencia
+        var imagenesSecuenciaConejo : [UIImage] = []
+        imagenesSecuenciaConejo.append(UIImage(named: "Conejo1")!)
+        imagenesSecuenciaConejo.append(UIImage(named: "Conejo2")!)
+        imagenesSecuenciaConejo.append(UIImage(named: "Conejo3")!)
+        imagenesSecuenciaConejo.append(UIImage(named: "Conejo4")!)
+        imagenesSecuenciaConejo.append(UIImage(named: "Conejo5")!)
+        imagenesSecuenciaConejo.append(UIImage(named: "Conejo6")!)
+        imagenesSecuenciaConejo.append(UIImage(named: "Conejo7")!)
+        
+        imgSecuencia.animationImages = imagenesSecuenciaConejo
+        imgSecuencia.animationDuration = 1
+        
     }
 
     override func didReceiveMemoryWarning() {
